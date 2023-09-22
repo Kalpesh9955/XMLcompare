f26604f8cf4592bbfbc093ad5923759e738f735d
Vivek0o <kr.vivekumar8@gmail.com>
2023-09-22 20:34:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-let xmlData;
-let excelData;
-
 function parseXML(file) {
-    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
 
-    reader.onload = function(e) {
-        const xmlString = e.target.result;
-        const parser = new DOMParser();
-        xmlData = parser.parseFromString(xmlString, "text/xml");
-    };
+        reader.onload = function(e) {
+            const xmlString = e.target.result;
+            const parser = new DOMParser();
+            const data = parser.parseFromString(xmlString, "text/xml");
+            resolve(data);
+        };
 
-    reader.readAsText(file);
+        reader.onerror = function(e) {
+            reject("Error reading XML file.");
+        };
+
+        reader.readAsText(file);
+    });
 }
 
 function parseExcel(file) {
-    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
 
-    reader.onload = function(e) {
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, { type: 'array' });
+        reader.onload = function(e) {
+            const data = new Uint8Array(e.target.result);
+            const workbook = XLSX.read(data, { type: 'array' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const jsonData = XLSX.utils.sheet_to_json(sheet);
+            resolve(jsonData);
+        };
 
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
+        reader.onerror = function(e) {
+            reject("Error reading Excel file.");
+        };
 
-        excelData = XLSX.utils.sheet_to_json(sheet);
-    };
-
-    reader.readAsArrayBuffer(file);
+        reader.readAsArrayBuffer(file);
+    });
 }
 
 function compareFiles() {
@@ -38,14 +47,12 @@ function compareFiles() {
         return;
     }
 
-    parseXML(xmlFile);
-    parseExcel(excelFile);
-
-    setTimeout(() => {  // Simple way to wait for async operations, better to use Promise
+    Promise.all([parseXML(xmlFile), parseExcel(excelFile)])
+    .then(([xml, excel]) => {
         let result = 'Comparison Results:<br>';
 
-        const foodFromXml = xmlData.getElementsByTagName('food')[0];
-        const foodFromExcel = excelData[0];
+        const foodFromXml = xml.getElementsByTagName('food')[0];
+        const foodFromExcel = excel[0];
 
         if (foodFromExcel.name !== foodFromXml.getElementsByTagName('name')[0].textContent) {
             result += 'Name does not match!<br>';
@@ -56,5 +63,8 @@ function compareFiles() {
         // Continue with other comparisons...
 
         document.getElementById('results').innerHTML = result;
-    }, 1000);  // Assuming it won't take more than 1 second to parse files
+    })
+    .catch(error => {
+        console.error("An error occurred:", error);
+    });
 }
